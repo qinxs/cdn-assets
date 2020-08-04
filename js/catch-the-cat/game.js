99d52beb076debb870e0e0f4526ed904ace882c9
qinxs <qin_xs@sina.cn>
2020-08-04 14:23:33 +0800

    update

--- js/catch-the-cat/game.js
+++ js/catch-the-cat/game.js
@@ -1,6 +1,6 @@
 !function() {
-  const is_mobi = navigator.userAgent.toLowerCase().match(/(ipod|ipad|iphone|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|wince)/i) != null;
-  var radius = is_mobi ? 12 : 20;
+  const isMobile = /mobile/i.test(window.navigator.userAgent);
+  var radius = isMobile ? 12 : 20;
   window.game = new CatchTheCatGame({ 
       w: 11, 
       h: 11, 
